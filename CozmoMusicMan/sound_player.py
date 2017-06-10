e4526aabb7240a46826eb716082d61a0b493716b
gus3000 <gus3000spam@gmail.com>
2017-06-10 03:06:09 +0200

    Sonate au clair de Push

---
+++ CozmoMusicMan/sound_player.py
@@ -0,0 +1,19 @@
+import pygame
+import os
+import time
+
+
+class Sound:
+
+    def __init__(self):
+        pygame.mixer.init()
+        pygame.mixer.pre_init()
+        dir_path = os.path.dirname(os.path.realpath(__file__))
+        self.s = []
+        self.s.append(pygame.mixer.Sound(dir_path + "\\Sound\\1.wav"))
+        self.s.append(pygame.mixer.Sound(dir_path + "\\Sound\\2.wav"))
+        self.s.append(pygame.mixer.Sound(dir_path + "\\Sound\\3.wav"))
+
+    def play(self, id):
+        self.s[id].play()
+        time.sleep(1)
