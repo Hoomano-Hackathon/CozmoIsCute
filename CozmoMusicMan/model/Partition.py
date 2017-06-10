736b4bb28a9f86def3df3f261563eafcdb43b2bf
Renaud <r.dharreville@gmail.com>
2017-06-11 00:34:22 +0200

    model

---
+++ CozmoMusicMan/model/Partition.py
@@ -0,0 +1,54 @@
+from CozmoMusicMan.model.sound_player import Sound
+
+class Note:
+
+    def __init__(self, note: int, duration: int=2):
+        self.note = note
+        self.duration = duration
+
+class Partition:
+
+    def __init__(self, speed: int=60, notes: [Note]=None, note_int: [int]=None):
+        self.speed = speed
+        self.notes = None
+        self.sound = Sound()
+        if notes:
+            self.notes = notes
+        elif note_int:
+            self.notes = Partition.pationning_with_int(note_int)
+
+    def pationning_with_int(cls, note_int: [int]):
+        part = []
+        for n in note_int:
+            part.append(Note(n))
+        return part
+
+    def play_part(self):
+        for n in self.notes:
+            self.sound.play(n, (n.duration * 60) / (2 * self.speed))
+
+    @staticmethod
+    def partitionning(self, tab_part: [[int]]):
+        part = [Note]
+        for t in tab_part:
+            if len(t) == 2:
+                part.append(Note(t[0], t[1]))
+        return part
+
+
+
+
+tab = Partition.partitionning(Partition, [[0, 1],
+                               [0, 1],
+                               [0, 1],
+                               [1, 1],
+                               [2, 2],
+                               [1, 1],
+                               [0, 1],
+                               [2, 2],
+                               [1, 1],
+                               [1, 1],
+                               [0, 1]])
+
+p = Partition(90, notes=tab)
+p.play_part()
---
+++ CozmoMusicMan/model/sound_player.py
@@ -0,0 +1,22 @@
+import pygame
+import os
+import time
+
+
+NB_MUSICS = 64
+
+class Sound:
+
+    def __init__(self):
+        pygame.mixer.init()
+        pygame.mixer.pre_init()
+        dir_path = os.path.dirname(os.path.realpath(__file__))
+        self.s = []
+        files = os.listdir(os.path.join(dir_path, "Sound"))
+        files.sort()
+        for filename in files:
+            self.s.append(pygame.mixer.Sound(os.path.join(dir_path, "Sound", filename)))
+        # print(self.s)
+
+    def play(self, id, duration):
+        self.s[id].play(maxtime=duration, fade_ms=250)
